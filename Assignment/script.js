d2a244770ffb1e85327a1ef5ce5b2f1b55a11c1f
eva25 <EVA@Eva.local>
2015-06-29 19:29:56 -0700

    Comp 1850 Projects
    - Assignments
    - Homeworks
    —— HTML, CSS, Javasript, JQuery

---
+++ Assignment/script.js
@@ -0,0 +1,12 @@
+//navigation tab scripts
+
+document.getElementById("navigation").innerHTML =
+	"<ul>"+
+			"<li><a href='index.html'>HOME</a></li>" +
+			"<li><a href='tags.html'>HTML TAGS</a></li>" +			
+			"<li><a href='resources.html'>RESOURCES</a></li>" +
+			"<li><a href='tutorials.html'>TUTORIAL</a></li>" +
+			"<li><a href='../Homework/index.html/index.html'>HOMEWORKS</a></li>" +
+			"<li><a href='../Homework03/index.html'>HOMEWORK03</a></li>" +
+			"<li><a href='../Assignment2/project_proposal.html'>ASSIGNMENT02</a></li>" +
+	"</ul>";
